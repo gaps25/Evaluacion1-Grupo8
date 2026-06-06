4b44f9a9ad7c420c3e7b6af48fb23a4e8a897391
Brayan <brayanazuaje11@gmail.com>
2026-06-06 01:13:37 -0400

    Docs: Añadir comentarios explicativos al codigo fuente

--- main.cpp
+++ main.cpp
@@ -10,6 +10,14 @@ int main() {
     cout << "   BIENVENIDO A LA EVALUACION DEL GRUPO 8         " << endl; 
     cout << "==================================================" << endl;
     cout << "Entorno virtualizado universal ejecutandose en Docker.\n" << endl;
+    
+    // Le pedimos al usuario que ingrese su nombre
+    cout << "Por favor, ingresa tu nombre para continuar: ";
+    cin >> nombreUsuario; // Guardamos lo que escriba el usuario en la variable nombreUsuario
+    
+    // Saludamos al usuario con su nombre y confirmamos que no hubo errores
+    cout << "\nHola, " <<nombreUsuario << " El programa se ha ejecutado sin errores." << endl;
+    cout << "Compilado con GCC dentro de un contenedor Linux Aislado.\n" << endl;
 
     return 0;
 }
