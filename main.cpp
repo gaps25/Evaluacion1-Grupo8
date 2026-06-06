149269726ad4c4b38454e7216ca3287d8dd6e857
Gabriel Palacios <gaps.enero25@gmail.com>
2026-06-05 23:05:11 -0400

    Feat: Agregar estructura inicial de bienvenida para el Grupo 8

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string nombreUsuario;
+
+    cout << "==================================================" << endl;
+    cout << "   BIENVENIDO A LA EVALUACION DEL GRUPO 8         " << endl; 
+    cout << "==================================================" << endl;
+    cout << "Entorno virtualizado universal ejecutandose en Docker.\n" << endl;
+
+    return 0;
+}
